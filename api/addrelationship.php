788216a219f4df6a062889e6edf12a6d4d0ba4de
xuxin3101 <xuxin3101@gmail.com>
2019-01-27 11:26:10 +0800

    添加添加关系的api

---
+++ api/addrelationship.php
@@ -0,0 +1,26 @@
+﻿<?php
+include('connect.php');
+if (!empty($_POST['superior'])) {
+    $superior=addslashes($_POST['superior']);
+    $subordinate=addslashes($_POST['subordinate']);
+    $password=addslashes($_POST['password']);
+    //上级与下级不允许相同
+    if ($superior==$subordinate) {
+        echo 'superior can not self';
+        return ;
+    }
+    //先验证密码
+    $sql="select * from users where username='$subordinate' and password='$password'";
+    $res= $mysqli->query($sql);
+    $row= $res->fetch_assoc();
+    if ($row) {//密码验证通过
+        $sql = "insert into relationship(id,superior,subordinate) values (null,'$superior','$subordinate');";
+        $res= $mysqli->query($sql);
+        echo $res;
+    } else {
+        echo 'erro';
+    }
+} else {
+    echo "erro";
+}
+$mysqli->close();
