9c1e3a3a8802488a0e00b0cdb77182cb7596b73c
xuxin3101 <xuxin3101@gmail.com>
2019-01-27 11:31:47 +0800

    查询自己是否有上级api

---
+++ api/checksuperior.php
@@ -0,0 +1,18 @@
+﻿<?php
+include('connect.php');
+if (!empty($_POST['subordinate'])) {
+    $subordinate=addslashes($_POST['subordinate']);
+    //查询自己的上级
+    $sql="select * from relationship where subordinate='$subordinate';";
+    $res=$mysqli->query($sql);
+    $row=$res->fetch_assoc();
+    if($row){
+        echo '1';
+    }else{
+        echo '0';
+    }
+  
+} else {
+    echo "erro";
+}
+$mysqli->close();
